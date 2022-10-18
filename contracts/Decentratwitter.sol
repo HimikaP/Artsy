413237625a291882fc4de8278f2374aa67a9eed1
HimikaP <prabhat.himika@gmail.com>
2022-10-18 20:10:19 +0530

    added test and contract

--- contracts/Decentratwitter.sol
+++ contracts/Decentratwitter.sol
@@ -1,12 +1,47 @@
 //SPDX-License-Identifier: MIT
 
+
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Decentratwitter is ERC721URIStorage {
     uint256 public tokenCount;
+    uint256 public postCount;
+    mapping(uint256 => Post) public posts;
+    // address --> nft id
+    mapping(address => uint256) public profiles;
+
+    struct Post {
+        uint256 id;
+        string hash;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event PostCreated(
+        uint256 id,
+        string hash,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event PostTipped(
+        uint256 id,
+        string hash,
+        uint256 tipAmount,
+        address payable author
+    );
 
     constructor() ERC721("Decentratwitter", "DAPP") {}
+
+    function mint(string memory _tokenURI) external returns (uint256) {
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        setProfile(tokenCount);
+        return (tokenCount);
+    }
 }
